62382c54267a1eeecbf3a76b3cd9f9039a2e17c8
Eyioyo Omatsola <eyo.omat@gmail.com>
2022-02-08 08:47:30 +0000

    :clown_face: Added WebSecurity configurations

--- src/main/java/com/eyo/authservice/registration/RegistrationService.java
+++ src/main/java/com/eyo/authservice/registration/RegistrationService.java
@@ -1,7 +1,10 @@
 package com.eyo.authservice.registration;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class RegistrationService {
     public String register(RegistrationRequest request) {
-        return null;
+        return "it works";
     }
 }
